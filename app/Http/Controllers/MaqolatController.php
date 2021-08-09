c3b912f7657d499766c3844103b8543281034859
unknown <bakiouisohail@gmail.com>
2021-08-09 01:00:44 +0100

    First Modification

--- app/Http/Controllers/MaqolatController.php
+++ app/Http/Controllers/MaqolatController.php
@@ -14,11 +14,10 @@ class MaqolatController extends Controller
 {
     //
     public function GetMaqolat(){
-       // echo 'i am from Conatact ';
+      
         $client = new Client();
 
-     /*   $res = $client->request('GET', 'https://type.fit/api/quotes');
-     $rs=Http::get('http://dummy.restapiexample.com/api/v1/employees"');*/
+
      $api_url = 'https://type.fit/api/quotes';
 
      
@@ -30,15 +29,11 @@ class MaqolatController extends Controller
     }
 
     public function Get_Picture($name){
-/*$response = Http::withHeaders([
-    'x-rapidapi-key' => '8dc8319816msh35e6ae096ea8286p1f71d4jsn8c7bb7a90601',
-    'x-rapidapi-host' => 'bing-image-search1.p.rapidapi.com'
-])->get("https://bing-image-search1.p.rapidapi.com/images/search",["q"=>$name])->json();*/
+
 $client = new Client();
-//$response = $client->request('GET',' https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json')->json();
+
 $rs=Http::get('https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles='.$name);
-//$api_url = 'https://nl.wikipedia.org/wiki/Eleanor_Roosevelt';
-  //  $response_data = json_decode($json_data);
+
      return $rs;
         
     }
